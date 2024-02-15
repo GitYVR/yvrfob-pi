e7b8fb497be827039f1aca1410cd96b952c7c6a8
GitYVR <124769744+GitYVR@users.noreply.github.com>
2024-02-15 10:58:05 -0800

    Use Covalent to get fob expiration (#2)
    * use fob nft
    
    * Read fob from covalent
    
    ---------
    
    Co-authored-by: Andrew Ori <ori.wagmi@gmail.com>
    Co-authored-by: ori-wagmi <97362935+ori-wagmi@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # yvrfob-pi
-Fob reader on the raspberry pi
+Fob reader on the raspberry pi. This service is managed by PM2 and handles unlocking the door on fob tap. 
 
-This service is managed by PM2 and handles unlocking the door on fob tap. The service will check the expiration in the FobNFT contract (0xc378418f2c30dfC73058a9d5018C3bA76910b31F) on Sepolia Testnet 
+Whenever someone taps the their fob on the door, this service will do the following:
+1. check the fobCache
+2. check Covalent
+3. check the blockchain directly
+
+The fob expiration lives in FobNFT contract deployed on Sepolia: https://github.com/ori-wagmi/DCTRLMEMBERSHIP. Users can register their fob at https://dctrl.wtf.
+
+## How to run
+To restart the fob service, run: `pm2 restart yvrfob`
+See logs with: `pm2 log`
+To see running pm2 services: `pm2 list`
+
+Run locally by removing `reader` from the code and running `node index.js`. 
+
+## .env
+Rename `.env.example` and set the expected fields. `LARGEPRIME` is defined in dctrl.wtf
--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const evdev = require("evdev");
 const reader = new evdev();
 const { Gpio } = require('onoff');
 const { ethers } = require('ethers');
+const { CovalentClient } = require("@covalenthq/client-sdk");
 require('dotenv').config()
+
+const client = new CovalentClient(process.env.COVALENT_KEY);
+const FOB_ABI = require("./FobABI.json");
+const FOBNFT_ADDRESS = "0xc378418f2c30dfC73058a9d5018C3bA76910b31F";
+const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC);
+const fobContract = new ethers.Contract(FOBNFT_ADDRESS, FOB_ABI, provider);
+const fobCache = {}; // key: fobnumber, value: expiration
 
 const openDoor = () => {
   try {
@@ -16,31 +24,87 @@ const openDoor = () => {
   }
 };
 
-reader.open(
-  "/dev/input/by-id/usb-Sycreader_RFID_Technology_Co.__Ltd_SYC_ID_IC_USB_Reader_08FF20140315-event-kbd"
-);
+// Call once on start. Instantiates the fobCache
+async function CallOnce_CreateFobCache() {
+  try {
+      for await (const resp of client.NftService.getTokenIdsForContractWithMetadata("eth-sepolia", FOBNFT_ADDRESS, {"withUncached": true})) {
+          // decode base64 fob token_url and extract expiration
+          let base64String = resp.nft_data.token_url.split(",")[1];
+          let decodedString = Buffer.from(base64String, "base64");
+          let jsonObject = JSON.parse(decodedString);
+          fobCache[resp.nft_data.token_id] = jsonObject.expiration;
+      }
+      console.log("CallOnce_CreateFobCache success. Number fobs cached: " + Object.keys(fobCache).length);
 
-let curFob = "";
+  } catch (error) {
+      console.log("CallOnce_CreateFobCache failed with: " + error.message);
+  }
+}
 
-const FOB_ABI = require("./FobABI.json");
-const FOBNFT_ADDRESS = "0xc378418f2c30dfC73058a9d5018C3bA76910b31F";
-const provider = new ethers.JsonRpcProvider("https://ethereum-sepolia.publicnode.com");
-const fobContract = new ethers.Contract(FOBNFT_ADDRESS, FOB_ABI, provider);
+// Returns true of `expiration > now`, otherwise false
+// We try 3 times to get the expiration:
+//   1. local fobCache
+//   2. Covalent `https://www.covalenthq.com/docs/api/nft/get-nft-token-ids-for-contract-with-metadata/`
+//   3. Fob contract via direct blockchain read 
+async function checkExpiration(key) {
+  const now = BigInt(new Date().getTime()) / BigInt(1000);
 
-const onReadKey = async (key) => {
+  if ((key in fobCache) && (fobCache[key] > now)) {
+      console.log("Got expiration from fobCache");
+      return true;
+  }
+
+  try {
+    const resp = await client.NftService.getNftMetadataForGivenTokenIdForContract("eth-sepolia", FOBNFT_ADDRESS, key, {"withUncached": true});
+    if (resp.data.items[0].nft_data.token_url !== "") {
+      let base64String = resp.data.items[0].nft_data.token_url.split(",")[1];
+      let decodedString = Buffer.from(base64String, "base64");
+      let jsonObject = JSON.parse(decodedString);
+
+      if (jsonObject.expiration > now) {
+        console.log("Got expiration from Covalent", key);
+        fobCache[key] = jsonObject.expiration;
+        return true;
+      }
+    } 
+  } catch (error) {
+    console.log("getNftMetadataForGivenTokenIdForContract failed with: " + error.message);
+  }
+
+  try {
+    const expiration = await fobContract.idToExpiration(key);
+
+    if (expiration > now) {
+      console.log("Got expiration from fobContract");
+      fobCache[key] = expiration;
+      return true;
+    }
+  } catch (error) {
+    console.log("fobContract.getExpiration failed with: " + error.message);
+  }
+
+  return false;
+}
+
+async function onReadKey(key) {
   console.log(`[FOB] ${key} detected`)
   try {
     const encryptedKey = BigInt(key) * BigInt(process.env.LARGEPRIME);
-    const expiration = await fobContract.idToExpiration(encryptedKey);
-    const now = new Date().getTime();
-    if ((expiration * BigInt(1000)) > BigInt(now)) {
+    if(await checkExpiration(encryptedKey)) {
       console.log("opening door");
       openDoor();
+    }
+    else {
+      console.log("fob expired or not found");
     }
   } catch(e) {
     console.log('ERROR onReadKey', e);
   }
 };
+
+reader.open(
+  "/dev/input/by-id/usb-Sycreader_RFID_Technology_Co.__Ltd_SYC_ID_IC_USB_Reader_08FF20140315-event-kbd"
+);
 
 reader.on("EV_KEY", function (data) {
   if (data.value !== 1) return;
@@ -48,8 +112,8 @@ reader.on("EV_KEY", function (data) {
 
   if (key === "ENTER") {
     onReadKey(curFob);
-    curFob = "";
-  } else {
-    curFob += key;
   }
 });
+
+console.log("ready!");
+CallOnce_CreateFobCache();
